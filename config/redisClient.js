5afc7c03a9d45278aff5242abf5ef5ab92f6f19a
PabloPerugini <pablochristian.perugini@estudiantes.unahur.edu.ar>
2026-06-15 19:48:42 -0300

    cambios

---
+++ config/redisClient.js
@@ -0,0 +1,16 @@
+const { createClient } = require('redis');
+require('dotenv').config();
+
+const cache = createClient({
+    url: process.env.REDIS_URL || 'redis://localhost:6379', // URL de conexión a Redis, configurable a través de variables de entorno
+    disableOfflineQueue: true, 
+    socket: {
+        connectTimeout: 1500 
+    }
+});
+
+cache.on('error', (err) => {
+    console.error('Redis status: Offline');
+});
+
+module.exports = cache;
