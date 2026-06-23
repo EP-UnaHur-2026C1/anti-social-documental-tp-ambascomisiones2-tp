b9986a93f4251a54c145d8342baa61a8118bf468
Valentinabertinat <valen.bertinat@gmail.com>
2026-06-23 20:07:53 -0300

    correcciones

--- middlewares/validateCommentExists.js
+++ middlewares/validateCommentExists.js
@@ -1,23 +1,23 @@
 const Post = require("../models/post");
-
+ 
 const validateCommentExists = async (req, res, next) => {
   try {
     const {idComment} = req.params
     const { postId} = req.body;
-
+ 
     const post = await Post.findById(postId);
     if (!post) {
       return res.status(404).json({ message: "Post asociado no encontrado" });
     }
-
+ 
     const foundComment = post.comments.id(idComment);
     if (!foundComment) {
-      console.log(commentId)
+ 
       return res
         .status(404)
         .json({ message: "Comentario no encontrado en este post" });
     }
-
+ 
     req.foundPost = post;
     req.foundComment = foundComment;
     next();
@@ -28,5 +28,5 @@ const validateCommentExists = async (req, res, next) => {
     });
   }
 };
-
+ 
 module.exports = validateCommentExists;
