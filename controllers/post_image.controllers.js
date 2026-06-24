d5ff1b843c47685859de8ccc7d004a30d9f29c86
Valentinabertinat <valen.bertinat@gmail.com>
2026-06-23 22:52:45 -0300

    GET imagenes por post

--- controllers/post_image.controllers.js
+++ controllers/post_image.controllers.js
@@ -2,22 +2,17 @@ const Post = require("../models/post");
 const fs = require("fs");
 const path = require("path");
 const cache = require("../config/redisClient");
-
+ 
 const createImage = async (req, res) => {
   try {
     if (!req.file)
       return res.status(400).json({ message: "No se subio ninguna imagen." });
-
     const { postId } = req.body;
     const post = await Post.findById(postId);
-
     if (!post) return res.status(404).json({ message: "Post no encontrado" });
-
     const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
-
     post.images.push({ imageUrl });
     await post.save();
-
     if (cache.isReady) {
       await cache.del(`post:${postId}`);
       await cache.del("posts:all");
@@ -29,27 +24,35 @@ const createImage = async (req, res) => {
       .json({ message: " Error al subir la imagen.", error: error.message });
   }
 };
-
+ 
+const getImagesByPost = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const post = await Post.findById(postId);
+    if (!post) return res.status(404).json({ message: "Post no encontrado" });
+    res.status(200).json(post.images);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error al obtener las imágenes del post", error: error.message });
+  }
+};
+ 
 const deleteImage = async (req, res) => {
   try {
     const { postId, imageId } = req.params;
-
     const post = await Post.findById(postId);
     if (!post) return res.status(404).json({ message: "Post no encontrado" });
-
     const image = await post.images.id(imageId);
     if (!image)
       return res
         .status(404)
         .json({ message: "Imagen no encontrada en este post" });
-
     const filename = path.basename(image.imageUrl);
     const filePath = path.join(__dirname, "..", "public", "uploads", filename);
     if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
-
     post.images.pull({ _id: imageId });
     await post.save();
-
     if (cache.isReady) {
       await cache.del(`post:${postId}`);
       await cache.del("posts:all");
@@ -61,19 +64,5 @@ const deleteImage = async (req, res) => {
       .json({ message: "Error al eliminar la imagen.", error: error.message });
   }
 };
-
-/*
-const getImagesByPost = async (req, res) => {
-  try {
-    const { postId } = req.params;
-    const post = await Post.findById(postId);
-    if (!post) return res.status(404).json({ message: "Post no encontrado" });
-
-    res.status(200).json(post.images);
-  } catch (error) {
-    res.status(500).json({ message: "Error al obtener las imágenes del post", error: error.message });
-  }
-};
-*/ // Lo hiciste Ari
-
-module.exports = { createImage, deleteImage };
+ 
+module.exports = { createImage, getImagesByPost, deleteImage };
